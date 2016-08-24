b22e535d79f3c98eeae12f0a79ce0ea6d31d49f7
Yiying Wang <madlogos@users.noreply.github.com>
2016-08-24 18:10:47 +0800

    js['pie'] is not js

--- R/js.R
+++ R/js.R
@@ -2,7 +2,7 @@
 tooltipJS <- function(type) {
     # tooltipJS templates for echarts
     js <- list()
-    js[['time']] <- 'function (params) {
+    js[['time']] <- JS('function (params) {
     var date = new Date(params.value[0]);
     data = date.getFullYear() + "-"
         + (date.getMonth() + 1) + "-"
@@ -17,9 +17,9 @@ tooltipJS <- function(type) {
         return data + "<br/>"
         + params.value[1];
     }
-    }'
+    }')
 
-    js[['scatter']] <- 'function (params) {
+    js[['scatter']] <- JS('function (params) {
     var i;
     var text;
     if (params.seriesName === null || params.seriesName === ""){
@@ -44,18 +44,18 @@ tooltipJS <- function(type) {
             + params.name + " : " + params.value;
         }
     }
-    }'
+    }')
 
-    js[['chord_mono']] <- 'function (params) {
+    js[['chord_mono']] <- JS('function (params) {
     if (params.name && params.name.indexOf("-") != -1) {
     return params.name.replace("-", " " + params.seriesName + " ")
     }
     else {
     return params.name ? params.name : params.data.id
     }
-    }'
+    }')
 
-    js[['chord_multi']] <- 'function (params) {
+    js[['chord_multi']] <- JS('function (params) {
     if (params.indicator2) {    // is edge
     return params.indicator2 + " " +
     params.name + " " + params.indicator + " : " +
@@ -63,23 +63,23 @@ tooltipJS <- function(type) {
     } else {    // is node
     return params.name
     }
-    }'
+    }')
 
     js[['pie']] <- '{a} <br/>{b} : {c} ({d}%)'
 
-    js[['k']] <- 'function (params) {
+    js[['k']] <- JS('function (params) {
         var res = params[0].name;
         res += "<br/>  Open : " + params[0].value[0] +
         "  High : " + params[0].value[3];
         res += "<br/>  Close : " + params[0].value[1] +
         "  Low : " + params[0].value[2];
         return res;
-    }'
+    }')
 
-    js[['hist']] <- 'function (params){
+    js[['hist']] <- JS('function (params){
         return params.value[2] + "<br/>Count:" +
         params.value[1];
-    }'
+    }')
     switch(type,
            time = js$time, scatter = js$scatter, chord_mono = js$chord_mono,
            chord_multi = js$chord_multi, pie = js$pie, k = js$k,
